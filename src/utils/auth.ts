53414f180a8cc1c9b8998eb67741cf4f8e3a6623
bbarber <breyton.barber@gmail.com>
2026-01-27 20:58:31 -0700

    remove username/password verification

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -8,13 +8,10 @@ import * as schema from "../db/schema/auth-schema";
 export const auth = betterAuth({
   database: drizzleAdapter(db, { provider: "sqlite", schema }),
   plugins: [tanstackStartCookies(), username()],
-  emailAndPassword: {
-    enabled: true,
-  },
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }
-  }
+    },
+  },
 });
