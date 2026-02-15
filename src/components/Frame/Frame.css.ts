b57b60f6c1d1bb6150ee72ce91a0dc984f06771d
Breyton Barber <breyton.barber@gmail.com>
2026-02-14 21:13:39 -0700

    Tweak theme

--- src/components/Frame/Frame.css.ts
+++ src/components/Frame/Frame.css.ts
@@ -2,7 +2,7 @@ import { style } from "@vanilla-extract/css";
 import { themeVars } from "../Theme.css";
 
 export const navStyle = style({
-	backgroundColor: themeVars.color.surface.l2,
+	backgroundColor: themeVars.color.primary.l1,
 	color: themeVars.color.base.light,
 	height: "50px",
 	display: "flex",
@@ -14,5 +14,5 @@ export const navStyle = style({
 export const frameContentStyle = style({
 	padding: "20px",
 	flexGrow: 1,
-	backgroundColor: themeVars.color.surface.l1,
+	backgroundColor: themeVars.color.surface.l2,
 });
